bb6261f7cb873e2f3706bb2e410faf0df4cd70b5
John S <alucinare@gmail.com>
2017-03-31 22:17:41 +1100

    Change styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,5 @@ class ApplicationController < ActionController::Base
   # a Devise method will ask for the user to be signed in before they access any of the pages on the site. In the applications controller it will be applied site wide.
   before_action :authenticate_user!
 
-  # everything has to go through this first any 
+  # everything has to go through this first any
 end
--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,7 @@
 class ContactController < ApplicationController
+  # this is cool, it will skip the before_action that is in the applications controller
+  skip_before_action :authenticate_user!
+
   def index
   end
 
@@ -9,7 +12,7 @@ class ContactController < ApplicationController
       email =  params[:contact][:email]
       message = params[:contact][:message]
 
-      # if the email was sent or the message was present this will 
+      # if the email was sent or the message was present this will
       if email.present? || message.present?
         # Tell the UserMailer to send a welcome email after save
         ContactMailer.send_contact_email(email, message).deliver_now
